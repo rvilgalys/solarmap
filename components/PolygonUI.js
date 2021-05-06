94680ea25de0418a4c217f61dad2d49a4ec39717
Rim Vilgalys <vilgalys@gmail.com>
2021-05-06 10:45:58 -0400

    enabled hotkeys

--- components/PolygonUI.js
+++ components/PolygonUI.js
@@ -1,11 +1,34 @@
+import { useEffect } from "react";
+
 const PolygonUI = ({
   className = "",
   isDrawing = false,
   onDrawToggle,
   onClearShape,
 }) => {
+  useEffect(() => {
+    // adding hotkeys -- triggers on keyup to avoid repeats on hold
+    document.addEventListener("keyup", (event) => {
+      // avoid triggering on regular inputs
+      if (event.isComposing) {
+        return;
+      }
+
+      if (event.code === "KeyD") {
+        onDrawToggle();
+      }
+      if (event.code === "KeyC") {
+        onClearShape();
+      }
+    });
+  }, []);
+
   return (
     <div className={className + " w-full flex items-center justify-end"}>
+      <div className="flex flex-col mr-4 text-sm">
+        <span>Draw Shape (D): Draw your installation on the map.</span>
+        <span>Clear Shape (C): Clear current drawing.</span>
+      </div>
       <button
         className={
           "text-xl font-display mr-4 p-1 border-b" +
